ad243d2e52495d25a4b2298994429352eee3210c
YousefAbaas <168109759+YousefAbaas@users.noreply.github.com>
2026-08-30 04:46:09 -0700

    fix: serve media files in production

--- backend/Aidora/urls.py
+++ backend/Aidora/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
@@ -43,4 +44,10 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
